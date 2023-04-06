d0c8bd93d52360587cf59e8579ee24329704eb0e
Aston Jaison Fernandes <astifranki@gmail.com>
2023-04-06 21:24:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,17 +25,17 @@ for i in range(0, 1000):
   corpus.append(review)
 print(corpus)
 
-# Creating the Bag of Words model
+# Using Bag of Words model
 from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(max_features = 1500)
 X = cv.fit_transform(corpus).toarray()
 y = dataset.iloc[:, -1].values
 
-# Splitting the dataset into the Training set and Test set
+# Splitting of dataset
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
 
-# Training the Naive Bayes model on the Training set
+# Training the Naive Bayes model
 from sklearn.naive_bayes import GaussianNB
 classifier = GaussianNB()
 classifier.fit(X_train, y_train)
@@ -44,7 +44,6 @@ classifier.fit(X_train, y_train)
 y_pred = classifier.predict(X_test)
 print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
 
-# Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix, accuracy_score
 cm = confusion_matrix(y_test, y_pred)
 print(cm)
